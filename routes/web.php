b685b90883bfebe8652880247f6e0db03d5cc0bd
ibrahimaah <ibrahim.a.a.h.2017@gmail.com>
2025-01-29 14:26:41 +0300

    init dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/tmp', function () {
+    return view('dashboard.index');
 });
